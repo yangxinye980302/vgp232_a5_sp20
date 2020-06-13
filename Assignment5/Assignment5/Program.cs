27662d7facc62a344e38b618438752c162ebc29f
Yang Steve <2571079331@qq.com>
2020-06-12 22:34:49 -0700

    //program

--- Assignment5/Assignment5/Program.cs
+++ Assignment5/Assignment5/Program.cs
@@ -10,11 +10,26 @@ namespace Assignment5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Assignment 5");
 
-            // TODO: initialize the inventory//11111111111111
-            // Add a few items
-            // Remove a few items
-            // show the inventory.
+            Character myCharacter = new Character("Bob", RaceCatagory.Elf, 100);
+
+            myCharacter.TakeDamage(10);
+
+            myCharacter.RestoreHealth(10);
+
+            Console.WriteLine("The game has ended with {0} with {1} health", myCharacter.Name, myCharacter.HealthPoints);
+
+            Inventory items = new Inventory(5);
+            Item item1 = new Item("yang", 3, ItemType.consumable);
+            Item item2 = new Item("xinye", 4, ItemType.equipment);
+            Item item3 = new Item("leo", 5, ItemType.key);
+            items.Add(item1);
+            items.Add(item2);
+            items.Add(item3);
+    
+            items.Remove(item1);
+            items.ShowInventory();
 
         }
     }
